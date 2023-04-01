cb6325bfb30f974c0e2c020fe411bc71ee621333
AlbertoMarLor <amartinl92@gmail.com>
2023-04-01 11:02:58 +0200

    querys de characters

--- models/characters.model.js
+++ models/characters.model.js
@@ -2,6 +2,41 @@ const getAll = () => {
     return db.query('SELECT * FROM harry_potter.characters;');
 }
 
+const getById = (characterId) => {
+    return db.query('select * from characters where id = ?',
+        [characterId]
+    )
+}
+
+const updateById = (characterId, { name, type, image, status }) => {
+    return db.query(
+        `update characters set
+        name = ?,
+        type = ?,
+        image = ?,
+        status = ?
+        where id = ?`,
+        [name, type, image, status, characterId]
+    )
+}
 
 
-module.exports = { getAll }
+const create = ({ name, type, image, status }) => {
+    return db.query(`INSERT INTO harry_potter.characters
+    (name,
+    type,
+    image, 
+    status)
+    VALUES
+    (?,?,?,?);`,
+        [name, type, image, status]
+    )
+}
+
+const deleteById = (characterId) => {
+    return db.query('DELETE FROM harry_potter.characters WHERE id = ?',
+        [characterId]
+    )
+}
+
+module.exports = { getAll, updateById, getById, create, deleteById }
--- routes/api/characters.js
+++ routes/api/characters.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const { getAll } = require('../../models/characters.model');
+const { getAll, updateById, getById, create, deleteById } = require('../../models/characters.model');
 
 
 router.get('/', async (req, res) => {
@@ -17,5 +17,74 @@ router.get('/', async (req, res) => {
 
 
 });
+
+router.get('/:characterId', async (req, res) => {
+
+    const { characterId } = req.params;
+    try {
+        const [result] = await getById(characterId);
+        if (result.length === 0) {
+            return res.json({ fatal: 'No existe personaje con ese ID' })
+        }
+        res.json(result[0]);
+    } catch (error) {
+        res.json({ fatal: error.message })
+    }
+
+
+
+
+});
+
+
+router.post('/newCharacter', async (req, res) => {
+
+
+    try {
+        const [newCharacter] = await create(req.body);
+        const [character] = await getById(newCharacter.insertId);
+        res.json(character[0]);
+    } catch (error) {
+        res.json({ fatal: error.message })
+    }
+
+});
+
+
+
+router.put('/:characterId', async (req, res) => {
+
+    const { characterId } = req.params;
+
+    try {
+        await updateById(characterId, req.body);
+        const [character] = await getById(characterId);
+
+        res.json(character[0]);
+    } catch (error) {
+        res.json({ fatal: error.message })
+    }
+});
+
+
+router.delete('/:characterId', async (req, res) => {
+
+    const { characterId } = req.params;
+    try {
+        const [character] = await getById(characterId);
+        if (character.length === 0) {
+            return res.json({ fatal: 'No existe un personaje con ese ID' });
+
+        }
+
+        await deleteById(characterId);
+
+        res.json(character[0]);
+    } catch (error) {
+        res.json({ fatal: error.message })
+    }
+
+});
+
 
 module.exports = router;
